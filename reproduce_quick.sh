4ba142296b00066018485d03e2a6863bdbf4cbf2
Veda Sheersh Boorla <mailforveda@gmail.com>
2025-04-09 16:01:06 -0400

    Update reproduce_quick.sh

--- reproduce_quick.sh
+++ reproduce_quick.sh
@@ -3,5 +3,5 @@
 ./scripts/fig6_figS8.sh analysis
 python ./scripts/plot_figS9.py
 ./scripts/figS10_figS11.sh analysis
-python ./scripts/analyze_ensemble.py
+# python ./scripts/analyze_ensemble.py ## Has a bug currently #TODO
 ./scripts/figS3.sh analysis
